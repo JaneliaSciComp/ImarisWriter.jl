8b23b45cd29ffd8eeb3f55bfa72b12155803f979
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-03-25 20:16:01 -0400

    Use ImarisWriter_jll binaries

--- src/ImarisWriter.jl
+++ src/ImarisWriter.jl
@@ -1,6 +1,7 @@
 module ImarisWriter
 
 using Libdl
+using ImarisWriter_jll
 #using Lz4_jll, Zlib_jll, HDF5_jll
 
 # Need the following DLLs
@@ -15,12 +16,16 @@ using Libdl
 include("imariswriter_common.jl")
 include("imariswriter_api.jl")
 
-const bpImarisWriter96_path = joinpath( @__DIR__(), "..", "bpImarisWriter96.dll")
-const bpImarisWriter96 = :bpImarisWriter96
+#const bpImarisWriter96_path = joinpath( @__DIR__(), "..", "bpImarisWriter96.dll")
+#const bpImarisWriter96 = :bpImarisWriter96
+const bpImarisWriter96_path = ImarisWriter_jll.libbpImarisWriter96_path
+const bpImarisWriter96 = ImarisWriter_jll.libbpImarisWriter96
+const bpImarisWriter96_h = ImarisWriter_jll.libbpImarisWriter96_handle
 
 function __init__()
-    global bpImarisWriter96_h
-    bpImarisWriter96_h =  dlopen( bpImarisWriter96_path )
+    #global bpImarisWriter96_h
+    #bpImarisWriter96_h =  dlopen( bpImarisWriter96_path )
+    @info bpImarisWriter96_path
 end
 
 end
